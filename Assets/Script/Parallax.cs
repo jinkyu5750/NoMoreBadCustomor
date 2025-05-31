208f845bc026f3be65f0efa82bc479e35f0c0d78
jinkyu5750 <jinkyu57505750@gmail.com>
2025-05-31 15:28:41 +0900

    25.05.31 Endless Platform, Parallax 구현

---
+++ Assets/Script/Parallax.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startPos;
+    [SerializeField] private Camera cam;
+    [SerializeField] private float parallaxPower;
+    void Start()
+    {
+        cam = Camera.main;
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = cam.transform.position.x * (1 - parallaxPower);
+        float dist = (cam.transform.position.x * parallaxPower);
+
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length) startPos += length;
+        else if (temp < startPos - length) startPos -= length;
+    }
+}
