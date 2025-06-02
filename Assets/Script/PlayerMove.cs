c20c65e63accc7338ea5f626f36c2ce7ccafa554
jinkyu5750 <jinkyu57505750@gmail.com>
2025-06-02 15:13:23 +0900

    25.06.01

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -2,22 +2,23 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    enum attack { Running=1, Upper,Lower }
 
-    enum attack { Running,Upper,}
     private Animator ani;
     private Rigidbody2D rig;
     private CapsuleCollider2D col;
 
     private bool isRun = false;
+    private bool isAttack = false;
 
     [SerializeField] private float speed;
     private void Start()
     {
         ani = GetComponent<Animator>();
-        rig = GetComponent<Rigidbody2D>(); 
+        rig = GetComponent<Rigidbody2D>();
         col = GetComponent<CapsuleCollider2D>();
 
-        
+
     }
 
     private void Update()
@@ -28,8 +29,14 @@ public class PlayerMove : MonoBehaviour
             isRun = true;
         }
 
+        if(Input.GetKeyDown(KeyCode.Space) && ani.GetCurrentAnimatorStateInfo(0).IsName("Run"))
+        {
+              Attack(attack.Running);
+
+        }
 
         Move();
+     
     }
 
     private void Move()
@@ -38,9 +45,20 @@ public class PlayerMove : MonoBehaviour
             transform.Translate(new Vector2(1, 0) * Time.deltaTime * speed);
     }
 
-    private void Attack()
+    private void Attack(attack dir)
     {
-        ani.SetInteger("Attack",)
+      
+        switch (dir)
+        {
+           case attack.Running:
+                ani.SetInteger("Attack", (int)attack.Running);
+                break;
+            case attack.Upper:
+                break;
+            case attack.Lower:
+                break;
+
+        }
     }
 }
 
