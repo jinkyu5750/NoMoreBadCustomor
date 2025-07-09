09ac990e74d583bbd928521286228cab2704df81
jinkyu5750 <jinkyu57505750@gmail.com>
2025-07-09 17:32:29 +0900

    2025.07.09
    대쉬 리지드바디 작동 안함, (isRunning이 꺼져있을때는 되던데, StateMachineBehaviour확인해봐야할듯, 반응이 느린가?)

---
+++ Assets/PlayerStateMachineBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachineBehaviour : StateMachineBehaviour
+{
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+    {
+        // 달리기 -> 공격,피격
+        animator.GetComponent<Player>().isRunning = false;
+        animator.GetComponent<PlayerAttack>().SetCanAttack(false);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+   
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //공격,피격 -> 달리기
+        animator.GetComponent<Player>().isRunning = true;
+        animator.GetComponent<PlayerAttack>().SetCanAttack(true);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/Script/PlayerAttack.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class PlayerAttack : MonoBehaviour
+{
+    Camera_Shake_Zoom camera_Shake_Zoom;
+
+    private Player player;
+    public enum attack { Dash = 1, Upper, Lower }
+    private bool canAttack = true; 
+    
+    [SerializeField] private float dashTime = 0.5f;
+    [SerializeField] private float dashPower;
+
+    [SerializeField] private Vector2 attackBoxSize;
+    
+    public void InitPlayer(Player player)
+    {
+        this.player = player;
+    }
+    
+    
+    void Start()
+    {
+        camera_Shake_Zoom = GetComponent<Camera_Shake_Zoom>();
+    }
+
+   
+    public IEnumerator Attack(attack dir)
+    {
+        if (canAttack == false) yield return null;
+
+        player.components.ani.SetInteger("Attack", (int)dir);
+
+        switch (dir)
+        {
+            case attack.Dash:
+                {
+                    Debug.Log("asd");
+                    ParticleManager.instance.UseObject("DashDust",transform.position);
+                    GetComponent<GhostEffect>().IsGhostOn = true;
+                    float curTime = dashTime;
+
+                    player.components.rig.velocity = Vector2.right * dashPower;
+                    while (curTime > 0)
+                    {
+                        curTime -= Time.deltaTime;
+                        yield return null;
+                    }
+                    player.components.rig.velocity = new Vector2(player.runSpeed, player.components.rig.velocity.y);
+                    GetComponent<GhostEffect>().IsGhostOn = false;
+                    break;
+                }
+
+            case attack.Upper:
+                break;
+            case attack.Lower:
+                break;
+
+        }
+
+
+        yield return new WaitForSeconds(0.5f);
+        player.components.ani.SetInteger("Attack", 0);
+    }
+
+    public void AttackAniEvent(string attackDir)
+    {
+
+        Collider2D hit = new Collider2D();
+        switch (attackDir)
+        {
+            case "Dash":
+                hit = Physics2D.OverlapBox(transform.position + new Vector3(1, 1, 0), attackBoxSize, 0, LayerMask.GetMask("Enemy"));
+                break;
+            case "Upper":
+
+                break;
+            case "Lower":
+
+                break;
+
+        }
+        if (hit != null)
+        {
+            Debug.Log("¸Â¾Ò´Ù");
+            StartCoroutine(camera_Shake_Zoom.ShakeCam(2.5f, 1, 0.1f));
+            StartCoroutine(camera_Shake_Zoom.ZoomInCam());
+
+            Vector2 randomCircle = Random.insideUnitCircle * 0.5f;
+            // Instantiate(attackHitParticle, hit.transform.position + new Vector3(randomCircle.x, randomCircle.y, 0), Quaternion.identity);
+            ParticleManager.instance.UseObject("AttackHit", hit.transform.position + new Vector3(randomCircle.x, randomCircle.y, 0));
+            hit.gameObject.GetComponent<Enemy>().EnemyDead();
+
+        }
+
+
+
+    }
+
+
+    public void SetCanAttack(bool canAttack)
+    {
+        this.canAttack = canAttack;
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position + new Vector3(1, 1, 0), attackBoxSize);
+    }
+}
